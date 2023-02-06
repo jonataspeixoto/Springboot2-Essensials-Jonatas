12034c77c4f2596b1bd9573aaeba6662fd47a549
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-02-06 03:02:41 -0300

    Aula 16 Curso Springboot 2 Essentials do DevDojo

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -3,4 +3,10 @@ package academy.devdojo.springboot2.repository;
 import academy.devdojo.springboot2.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimeRepository extends JpaRepository<Anime,Long> { }
+import java.util.List;
+
+public interface AnimeRepository extends JpaRepository<Anime,Long> {
+
+    List<Anime> findByName(String name);
+
+}
