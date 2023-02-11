9e572d03daf76a3f4993556ee8429cc91d1f14cc
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-02-11 16:22:58 -0300

    Aula 22 Curso Springboot 2 Essentials do DevDojo

--- src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -1,26 +1,31 @@
 package academy.devdojo.springboot2.handler;
 
-import academy.devdojo.springboot2.exception.BadRequestException;
-import academy.devdojo.springboot2.exception.BadRequestExceptionDetails;
-import academy.devdojo.springboot2.exception.ValidationExceptionDetails;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.cglib.core.Local;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+import academy.devdojo.springboot2.exception.BadRequestException;
+import academy.devdojo.springboot2.exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2.exception.ExceptionDetails;
+import academy.devdojo.springboot2.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(
             BadRequestException exception){
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
@@ -31,9 +36,11 @@ public class RestExceptionHandler {
                         .developerMessage(exception.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
         String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
@@ -48,5 +55,20 @@ public class RestExceptionHandler {
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+        Exception exception, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Bad Request Exception, Check the Documentation")
+                .details(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>( exceptionDetails, headers, statusCode);
     }
 }
