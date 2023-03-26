a1092dc243b9dd0aa60e4262985b459295db012a
jonatas.peixoto.zup <jonatas.peixoto@zup.com.br>
2023-03-26 17:20:26 -0300

    Aula 40 Curso Springboot 2 Essentials do DevDojo

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package academy.devdojo.springboot2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -19,11 +22,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        CookieCsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http
+            .csrf()
+            .disable()
             .authorizeHttpRequests((authz) -> authz
                 .anyRequest()
                 .authenticated()
             )
+//            .csrf((csrf) -> csrf
+//                .csrfTokenRepository(tokenRepository)
+//                .csrfTokenRequestHandler(requestHandler)
+//            )
             .httpBasic();
         return http.build();
     }
